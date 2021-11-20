1154fbcb0c3fba589fe58dbcb99c66c9e685efec
Jakub Działowy <dzialowy04@gmail.com>
2021-11-20 13:03:03 +0100

    Proper import of ARP to the ping program

--- ICMP/ping.py
+++ ICMP/ping.py
@@ -1,10 +1,18 @@
-from icmplib import multiping
+import os
+import sys
 
-list_of_ip = ['192.168.0.206', '192.168.0.31']
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from icmplib import multiping
+from ARP.arp_scanner import scan_arp
+
+list_of_ip = ['192.168.0.206', '192.168.0.102', '192.168.0.136']
 hosts = multiping(list_of_ip)
 
 for host in hosts:
     if host.is_alive:
-        print(f'{host.address} is up!')
+        scan_arp(host.address)
     else:
-        print(f'{host.address} is down!')
+        print(f'No response from: {host.address}')
